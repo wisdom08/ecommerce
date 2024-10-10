caae9c75b7db5aa759fd1f74e25d6f756bf9ce7e
wisdom <wisdom08@kakao.com>
2024-10-11 07:11:01 +0900

    공통 응답 처리 추가

---
+++ src/main/java/org/wisdom/ecommerce/common/exception/ApiControllerAdvice.java
@@ -0,0 +1,27 @@
+package org.wisdom.ecommerce.common.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.wisdom.ecommerce.common.model.ErrorResponse;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+class ApiControllerAdvice extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+        return ResponseEntity.status(500).body(new ErrorResponse(ex.getMessage()));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.status(400).body(new ErrorResponse(ex.getMessage()));
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<ErrorResponse> handleNoSuchElementException(NoSuchElementException ex) {
+        return ResponseEntity.status(404).body(new ErrorResponse(ex.getMessage()));
+    }
+}

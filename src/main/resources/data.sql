9f3006b997f57aa481430f526d51d983290fc906
wisdom <wisdom08@kakao.com>
2024-10-18 07:19:29 +0900

    데이터 셋팅

---
+++ src/main/resources/data.sql
@@ -0,0 +1,35 @@
+INSERT INTO ecommerce.product (name, price, quantity, created_at, updated_at) VALUES
+                                                                                  ('Laptop', 1000, 50, NOW(), NOW()),
+                                                                                  ('Headphones', 150, 200, NOW(), NOW()),
+                                                                                  ('Smartphone', 800, 100, NOW(), NOW()),
+                                                                                  ('Tablet', 500, 75, NOW(), NOW()),
+                                                                                  ('Smartwatch', 300, 60, NOW(), NOW());
+
+INSERT INTO ecommerce.user (email, created_at, updated_at) VALUES
+                                                               ('john.doe@example.com', NOW(), NOW()),
+                                                               ('jane.smith@example.com', NOW(), NOW()),
+                                                               ('alex.johnson@example.com', NOW(), NOW());
+
+
+INSERT INTO ecommerce.cart (user_id, created_at, updated_at) VALUES
+                                                                 (1, NOW(), NOW()),
+                                                                 (2, NOW(), NOW());
+
+INSERT INTO ecommerce.cart_item (cart_id, product_id, quantity, created_at, updated_at) VALUES
+                                                                                            (1, 1, 2, NOW(), NOW()),
+                                                                                            (1, 3, 1, NOW(), NOW()),
+                                                                                            (2, 2, 3, NOW(), NOW());
+
+INSERT INTO ecommerce.orders (user_id, status, created_at, updated_at) VALUES
+                                                                           (1, 'paid', NOW(), NOW()),
+                                                                           (2, 'preparing', NOW(), NOW());
+
+INSERT INTO ecommerce.order_item (order_id, product_id, quantity, price, created_at, updated_at) VALUES
+                                                                                                     (1, 1, 2, 1000, NOW(), NOW()),
+                                                                                                     (1, 3, 1, 800, NOW(), NOW()),
+                                                                                                     (2, 2, 3, 150, NOW(), NOW());
+
+
+INSERT INTO ecommerce.wallet (balance, user_id, created_at, updated_at) VALUES
+                                                                            (5000, 1, NOW(), NOW()),
+                                                                            (1000, 2, NOW(), NOW());
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,86 @@
+drop table if exists ecommerce.order_item;
+drop table if exists ecommerce.cart_item;
+drop table if exists ecommerce.orders;
+drop table if exists ecommerce.cart;
+drop table if exists ecommerce.product;
+drop table if exists ecommerce.transaction;
+drop table if exists ecommerce.wallet;
+drop table if exists ecommerce.user;
+
+create table ecommerce.product
+(
+    id         bigint auto_increment primary key,
+    name       varchar(255),
+    price      int,
+    quantity   int,
+    created_at timestamp,
+    updated_at timestamp
+);
+
+
+create table ecommerce.transaction
+(
+    id         bigint auto_increment primary key,
+    wallet_id  bigint,
+    amount     int,
+    type       enum ('charge', 'deduct'),
+    created_at timestamp,
+    updated_at timestamp
+);
+
+create table ecommerce.user
+(
+    id         bigint auto_increment primary key,
+    email      varchar(255),
+    created_at timestamp,
+    updated_at timestamp
+);
+
+create table ecommerce.cart
+(
+    id         bigint auto_increment primary key,
+    user_id    bigint,
+    created_at timestamp,
+    updated_at timestamp
+);
+
+create table ecommerce.cart_item
+(
+    id         bigint auto_increment primary key,
+    cart_id    bigint,
+    product_id bigint,
+    quantity   int,
+    created_at timestamp,
+    updated_at timestamp
+);
+
+create table ecommerce.orders
+(
+    id         bigint auto_increment primary key,
+    user_id    bigint,
+    status     enum ('paid', 'preparing', 'delivered'),
+    created_at timestamp,
+    updated_at timestamp
+);
+
+create table ecommerce.order_item
+(
+    id         bigint auto_increment primary key,
+    order_id   bigint,
+    product_id bigint,
+    quantity   int,
+    price      int,
+    created_at timestamp,
+    updated_at timestamp
+);
+
+create table ecommerce.wallet
+(
+    id         bigint auto_increment     primary key,
+    balance    int,
+    user_id    bigint,
+    created_at timestamp,
+    updated_at timestamp
+);
+
+

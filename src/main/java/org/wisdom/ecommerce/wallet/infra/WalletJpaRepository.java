c785c38d260101c56e139f72e668a79f569ae4ba
wisdom08 <wisdom08@kakao.com>
2024-10-18 09:44:11 +0900

    동시성 제어를 위한 락 추가

--- src/main/java/org/wisdom/ecommerce/wallet/infra/WalletJpaRepository.java
+++ src/main/java/org/wisdom/ecommerce/wallet/infra/WalletJpaRepository.java
@@ -1,17 +1,19 @@
 package org.wisdom.ecommerce.wallet.infra;
 
+import jakarta.persistence.LockModeType;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
 public interface WalletJpaRepository extends JpaRepository<WalletEntity, Long> {
 
     Optional<WalletEntity> findByUserId(long userId);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Transactional
     @Modifying
     @Query(value = "update WalletEntity w SET w.balance = :balanceCharged where w.id = :walletId")

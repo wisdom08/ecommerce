1d2e17a37bf42e92cb3933ed243e9b263c1c5573
wisdom08 <wisdom08@kakao.com>
2024-10-21 11:51:55 +0900

    Revert "유스케이스 개발 및 통합테스트 작성 "

--- src/main/java/org/wisdom/ecommerce/product/infra/ProductJpaRepository.java
+++ src/main/java/org/wisdom/ecommerce/product/infra/ProductJpaRepository.java
@@ -1,12 +1,10 @@
 package org.wisdom.ecommerce.product.infra;
 
-import jakarta.persistence.LockModeType;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
+
+import java.util.Optional;
 
 public interface ProductJpaRepository extends JpaRepository<ProductEntity, Long> {
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    Optional<ProductEntity> findById(long productId);
+  Optional<ProductEntity> findById(long productId);
 }

4793d1272e31f56b17f497867079739246601015
wisdom <wisdom08@kakao.com>
2024-11-06 21:31:53 +0900

    쓰지 않는 WalletRepositoryCustom 제거

--- src/main/java/org/wisdom/ecommerce/wallet/infra/WalletJpaRepository.java
+++ src/main/java/org/wisdom/ecommerce/wallet/infra/WalletJpaRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface WalletJpaRepository extends JpaRepository<WalletEntity, Long>, WalletRepositoryCustom {
+public interface WalletJpaRepository extends JpaRepository<WalletEntity, Long> {
 
   Optional<WalletEntity> findByUserId(Long userId);
 

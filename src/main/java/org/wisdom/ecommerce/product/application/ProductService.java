ff9e454733a71dcaca2b43b9339c4f700acc3c82
wisdom <wisdom08@kakao.com>
2024-11-10 12:06:01 +0900

    상품 상세 조회 API - Redis 캐시 적용

--- src/main/java/org/wisdom/ecommerce/product/application/ProductService.java
+++ src/main/java/org/wisdom/ecommerce/product/application/ProductService.java
@@ -1,22 +1,32 @@
 package org.wisdom.ecommerce.product.application;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.wisdom.ecommerce.product.domain.Product;
+import org.wisdom.ecommerce.product.infra.ProductRedisManager;
 
 @Service
 public class ProductService {
 
   private final ProductRepository productRepository;
+  private final ProductRedisManager productRedisManager;
 
-  public ProductService(ProductRepository productRepository) {
+  public ProductService(ProductRepository productRepository, ProductRedisManager productRedisManager) {
     this.productRepository = productRepository;
+    this.productRedisManager = productRedisManager;
   }
 
-  @Transactional(readOnly = true)
+  @Transactional
   public Product getProductBy(Long productId) {
-    return productRepository.getProductBy(productId);
+    return Optional.ofNullable(productRedisManager.get(productId))
+        .or(() -> Optional.ofNullable(productRepository.getProductBy(productId))
+            .map(product -> {
+              productRedisManager.save(product);
+              return product;
+            })
+        ).orElse(null);
   }
 
   @Transactional(readOnly = true)
@@ -28,5 +38,11 @@ public class ProductService {
   public void updateStock(Product product, Integer quantity) {
     product.validateQuantity(quantity);
     productRepository.updateStock(product, product.quantity() - quantity);
+    productRedisManager.update(Product.builder()
+        .id(product.id())
+        .name(product.name())
+        .price(product.price())
+        .quantity(product.quantity() - quantity)
+        .build());
   }
 }
